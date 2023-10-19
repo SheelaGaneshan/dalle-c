2d7ea7b7b39dae9669b94318eb6584d0767f0530
SheelaGaneshan <sheela12selenophile@gmail.com>
2023-10-19 15:46:01 -0700

    modified origin

--- backend/index.js
+++ backend/index.js
@@ -9,11 +9,8 @@ import imgaiRoute from "./routes/imgaiRoute.js";
 dotenv.config();
  
 const app = express();
-app.use(cors(
-    {
-        origin: ['https://ai-img-creator.onrender.com']
-    }
-));
+app.use(cors()
+);
 
 app.use(express.json({limit: '50mb'}));
 
