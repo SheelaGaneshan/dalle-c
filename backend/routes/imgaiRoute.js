c33b7c39f923660fa1531e7b90699e7c11db45c1
SheelaGaneshan <sheela12selenophile@gmail.com>
2023-10-19 10:41:58 -0700

    configuration changes

--- backend/routes/imgaiRoute.js
+++ backend/routes/imgaiRoute.js
@@ -1,16 +1,14 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
-import { Configuration, OpenAI } from 'openai';
+import OpenAI  from 'openai';
 
 dotenv.config();
 
 const router = express.Router();
 
-const configuration = new Configuration({
+const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
-})
-
-const openai = new OpenAI(configuration);
+});
 
 router.route('/').get((req, res)=>{
     res.send('OpenAI connected');
